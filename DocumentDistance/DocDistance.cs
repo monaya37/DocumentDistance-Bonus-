fa117ea941f9eea5b69b138876fff48e3c74e92f
Khaira Bah <Monayaa1637@gmail.com>
2024-02-13 19:30:13 +0200

    Add project files.

---
+++ DocumentDistance/DocDistance.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+namespace DocumentDistance
+{
+    class DocDistance
+    {
+        // *****************************************
+        // DON'T CHANGE CLASS OR FUNCTION NAME
+        // YOU CAN ADD FUNCTIONS IF YOU NEED TO
+        // *****************************************
+        /// <summary>
+        /// Write an efficient algorithm to calculate the distance between two documents
+        /// </summary>
+        /// <param name="doc1FilePath">File path of 1st document</param>
+        /// <param name="doc2FilePath">File path of 2nd document</param>
+        /// <returns>The angle (in degree) between the 2 documents</returns>
+        public static double CalculateDistance(string doc1FilePath, string doc2FilePath)
+        {
+            // TODO comment the following line THEN fill your code here
+            // throw new NotImplementedException();
+            string d1 = File.ReadAllText(doc1FilePath);
+            string d2 = File.ReadAllText(doc2FilePath);
+
+            string s = "[A-Za-z0-9]*";
+            Regex re = new Regex(s);
+            Console.Write("\nD1:\n");
+            Console.Write(d1);
+            Console.Write("\nD2:\n");
+            Console.Write(d2);
+            return 0;
+        }
+    }
+}
